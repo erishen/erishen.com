bda92ccede1b613ae94d7dfb5cc865975a45f476
Erishen <lei_sun@Ctrip.com>
2017-08-18 09:52:31 +0800

    add crawler/csdn.
    Summary: None

--- server/crawler.js
+++ server/crawler.js
@@ -8,7 +8,7 @@ const cheerio = require('cheerio');
 
 exports.index = function(app){
     app.get('/crawler', (req, res) => {
-        var links = ['cnodejs', 'cnodejs2'];
+        var links = ['cnodejs', 'csdn'];
         var content = utilIndex.getLinks('crawler', links);
         res.send(content);
     });
@@ -25,29 +25,41 @@ exports.index = function(app){
             }
 
             var $ = cheerio.load(sres.text);
-            var items = [];
-            $('#topic_list .topic_title').each((idx, element) => {
-                var $element = $(element);
-                items.push({
-                    title: $element.attr('title'),
-                    href: $element.attr('href')
-                });
-            });
-
             var content = [];
-            var itemsLen = items.length;
-            for(var i = 0; i < itemsLen; i++)
-            {
-                var itemObj = items[i];
-                var title = itemObj.title;
-                var href = url + itemObj.href;
-                content.push('<p><a href="' + href + '" target="_blank">' + title + '</a></p>');
-            }
+            var index = 0;
+            $('#topic_list .topic_title').each((idx, element) => {
+                index++;
+                var $element = $(element);
+                content.push('<p>' + index + '. <a href="' + (url + $element.attr('href')) + '" target="_blank">' + $element.attr('title') + '</a></p>');
+            });
             res.send(content.join(''));
         });
     });
 
-    app.get('/crawler/cnodejs2', (req, res) => {
-        res.send('cnodejs2');
+    app.get('/crawler/csdn', (req, res) => {
+        var url = 'http://www.csdn.net';
+        superagent.get(url).end((err, sres)=>{
+            if(err)
+            {
+                res.send(err);
+                return;
+            }
+            var $ = cheerio.load(sres.text);
+            var content = [];
+            var index = 0;
+            $('.news_list li a').each((idx, element) => {
+                index++;
+                var $element = $(element);
+                content.push('<p>' + index + '. <a href="' + $element.attr('href') + '" target="_blank">' + $element.attr('title') + '</a></p>');
+            });
+
+            $('.box.flow li h3 a').each((idx, element) => {
+                index++;
+                var $element = $(element);
+                content.push('<p>' + index + '. <a href="' + $element.attr('href') + '" target="_blank">' + $element.attr('title') + '</a></p>');
+            });
+
+            res.send(content.join(''));
+        });
     });
 };
